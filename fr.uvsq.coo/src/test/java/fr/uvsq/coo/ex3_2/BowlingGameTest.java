20e84348923b50664a10bceaeea5c1099a7b460e
uvsq21505682 <philani14@yahoo.fr>
2015-11-19 13:26:18 +0100

    fr.uvsq.coo

--- fr.uvsq.coo/src/test/java/fr/uvsq/coo/ex3_2/BowlingGameTest.java
+++ fr.uvsq.coo/src/test/java/fr/uvsq/coo/ex3_2/BowlingGameTest.java
@@ -24,6 +24,20 @@ public class BowlingGameTest extends TestCase {
  rollMany(17,0);
  assertEquals(16,g.score());
  }
+ public void testOneStrike() throws Exception {
+ rollStrike();
+ g.roll(3);
+ g.roll(4);
+ rollMany(16, 0);
+ assertEquals(24, g.score());
+ }
+ public void testPerfectGame() throws Exception {
+ rollMany(12,10);
+ assertEquals(300, g.score());
+ }
+ private void rollStrike() {
+ g.roll(10);
+ }
  private void rollSpare() {
  g.roll(5);
  g.roll(5);
