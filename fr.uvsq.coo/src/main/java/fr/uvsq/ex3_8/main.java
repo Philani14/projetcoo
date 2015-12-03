7f189d14c43a5946f9bce6bdbda3576aad6096e0
21505682 <21505682@uvsq.com>
2015-12-03 11:43:40 +0100

    tp5

---
+++ fr.uvsq.coo/src/main/java/fr/uvsq/ex3_8/main.java
@@ -0,0 +1,16 @@
+package fr.uvsq.ex3_8;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+
+/**public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Personnel personnel= new Personnel
+				.Builder("GLANON","Philippe","14-45-1993")
+				.num_tel(new ArrayList<Telephone>().add(new Telephone("21022151").getTelephone())).
+	}
+
+}
+*/
